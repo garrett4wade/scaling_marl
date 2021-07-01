5db834975d33c61f65935553422c2eb8c79cf763
garrett4wade <fuwth17@gmail.com>
2021-07-01 11:54:03 +0800

    rename document

---
+++ run_learner_node.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy as np
+import pathlib
+import yaml
+import torch
+import multiprocessing as mp
+from config import get_config
+from envs.starcraft2.StarCraft2_Env import StarCraft2Env
+from envs.env_wrappers import ShareDummyVecEnv, ShareSubprocVecEnv
+from envs.starcraft2.smac_maps import get_map_params
+from utils.buffer import LearnerBuffer
+from system.receiver import Receiver
+from torch.multiprocessing import JoinableQueue as TorchJoinableQueue
+from system.trainer import Trainer
+from utils.utils import log
+"""Train script for SMAC."""
+
+
+def parse_args(args, parser):
+    parser.add_argument('--map_name', type=str, default='3m', help="Which smac map to run on")
+    parser.add_argument("--add_move_state", action='store_true', default=False)
+    parser.add_argument("--add_local_obs", action='store_true', default=False)
+    parser.add_argument("--add_distance_state", action='store_true', default=False)
+    parser.add_argument("--add_enemy_action_state", action='store_true', default=False)
+    parser.add_argument("--add_agent_id", action='store_true', default=False)
+    parser.add_argument("--add_visible_state", action='store_true', default=False)
+    parser.add_argument("--add_xy_state", action='store_true', default=False)
+    parser.add_argument("--use_state_agent", action='store_true', default=False)
+    parser.add_argument("--use_mustalive", action='store_false', default=True)
+    parser.add_argument("--add_center_xy", action='store_true', default=False)
+
+    all_args = parser.parse_known_args(args)[0]
+
+    return all_args
+
+
+def make_example_env(all_args):
+    def get_env_fn(rank):
+        def init_env():
+            if all_args.env_name == "StarCraft2":
+                env = StarCraft2Env(all_args)
+            else:
+                print("Can not support the " + all_args.env_name + "environment.")
+                raise NotImplementedError
+            env.seed(all_args.seed + rank * 10000)
+            return env
+
+        return init_env
+
+    return ShareDummyVecEnv([get_env_fn(0)])
+
+
+def make_eval_env(trainer_id, all_args):
+    def get_env_fn(rank):
+        def init_env():
+            if all_args.env_name == "StarCraft2":
+                env = StarCraft2Env(all_args)
+            else:
+                print("Can not support the " + all_args.env_name + "environment.")
+                raise NotImplementedError
+            env.seed(all_args.seed * 50000 + rank * 10000 + 12345 * trainer_id)
+            return env
+
+        return init_env
+
+    if all_args.n_eval_rollout_threads == 1:
+        return ShareDummyVecEnv([get_env_fn(0)])
+    else:
+        return ShareSubprocVecEnv([get_env_fn(i) for i in range(all_args.n_eval_rollout_threads)])
+
+
+def main():
+    parser = get_config()
+    all_args = parse_args(sys.argv[1:], parser)
+    # overwrite default configuration using yaml file
+    if all_args.config is not None:
+        with open(all_args.config) as f:
+            all_args_dict = yaml.load(f, Loader=yaml.FullLoader)
+        for k, v in all_args_dict.items():
+            setattr(all_args, k, v)
+
+    if all_args.algorithm_name == "rmappo":
+        all_args.use_recurrent_policy = True
+    elif all_args.algorithm_name == 'mappo':
+        all_args.use_recurrent_policy = False
+    else:
+        raise NotImplementedError
+
+    # NOTE: this line may incur a bug
+    # torch.set_num_threads(all_args.n_training_threads)
+    if all_args.cuda_deterministic:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    run_dir = pathlib.Path('./log') / all_args.env_name
+    if all_args.env_name == 'StarCraft2':
+        run_dir /= all_args.map_name
+    run_dir = run_dir / all_args.algorithm_name / all_args.experiment_name
+    if not run_dir.exists():
+        os.makedirs(str(run_dir))
+
+    # seed
+    torch.manual_seed(all_args.seed)
+    torch.cuda.manual_seed_all(all_args.seed)
+    np.random.seed(all_args.seed)
+
+    example_env = make_example_env(all_args)
+    all_args.share_observation_space = example_env.share_observation_space[
+        0] if all_args.use_centralized_V else example_env.observation_space[0]
+    all_args.observation_space = example_env.observation_space[0]
+    all_args.action_space = example_env.action_space[0]
+
+    example_env.close()
+    del example_env
+
+    all_args.num_agents = get_map_params(all_args.map_name)["n_agents"]
+
+    buffer = LearnerBuffer(all_args, all_args.observation_space, all_args.share_observation_space,
+                           all_args.action_space)
+    nodes_ready_events = [mp.Event() for i in range(len(all_args.seg_addrs))]
+
+    recievers = [
+        Receiver(all_args, i, TorchJoinableQueue(), buffer, nodes_ready_events[i])
+        for i in range(len(all_args.seg_addrs))
+    ]
+    for r in recievers:
+        r.init()
+
+    trainers = [
+        Trainer(rank, buffer, all_args, nodes_ready_events, run_dir=run_dir) for rank in range(all_args.num_trainers)
+    ]
+    for trainer in trainers:
+        trainer.process.start()
+
+    for trainer in trainers:
+        trainer.process.join()
+    log.info('Trainers joined!')
+
+    for r in recievers:
+        r.close()
+    log.info('Receivers joined!')
+
+    log.info('Done!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ worker_node.py
@@ -0,0 +1,448 @@
+"""
+Algorithm entry poing.
+Methods of the APPO class initiate all other components (rollout & policy workers and learners) in the main thread,
+and then fork their separate processes.
+All data structures that are shared between processes are also created during the construction of APPO.
+
+This class contains the algorithm main loop. All the actual work is done in separate worker processes, so
+the only task of the main loop is to collect summaries and stats from the workers and log/save them to disk.
+
+Hyperparameters specific to policy gradient algorithms are defined in this file. See also algorithm.py.
+
+"""
+
+import math
+import multiprocessing
+import os
+import time
+from collections import deque
+from queue import Empty
+
+import torch
+from torch.multiprocessing import JoinableQueue as TorchJoinableQueue
+
+from system.actor_worker import ActorWorker
+from system.policy_worker import PolicyWorker
+from system.transmitter import Transmitter
+from utils.buffer import SharedWorkerBuffer
+from utils.timing import Timing
+from utils.utils import log, set_global_cuda_envvars, list_child_processes, kill_processes
+
+if os.name == 'nt':
+    from sample_factory.utils import Queue as MpQueue
+else:
+    from faster_fifo import Queue as MpQueue
+
+
+class ExperimentStatus:
+    SUCCESS, FAILURE, INTERRUPTED = range(3)
+
+
+# custom experiments can define functions to this list to do something extra with the raw episode summaries
+# coming from the environments
+EXTRA_EPISODIC_STATS_PROCESSING = []
+
+# custom experiments or environments can append functions to this list to postprocess some summaries, or aggregate
+# summaries, or do whatever else the user wants
+EXTRA_PER_POLICY_SUMMARIES = []
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class WorkerNode:
+    """Async PPO."""
+    def __init__(self, cfg, env_fn):
+
+        # we should not use CUDA in the main thread, only on the workers
+        set_global_cuda_envvars(cfg)
+        self.cfg = cfg
+
+        self.obs_space = cfg.observation_space
+        self.share_obs_space = cfg.share_observation_space
+        self.action_space = cfg.action_space
+        self.num_agents = cfg.num_agents
+
+        self.env_fn = env_fn
+
+        # shared memory allocation
+        self.buffer = SharedWorkerBuffer(self.cfg, self.obs_space, self.share_obs_space, self.action_space)
+
+        self.actor_workers = []
+        self.policy_workers = []
+        self.transmitters = []
+
+        # faster_fifo queue is initialized using BYTES!
+        self.report_queue = MpQueue(40 * 1000 * 1000)
+        # TODO: policy queue -> policy queues to support PBT
+        self.policy_queues = [MpQueue() for _ in range(self.cfg.num_policy_workers)]
+
+        # TODO: here we only consider actions for policy-sharing
+        act_shape = (self.cfg.envs_per_actor // self.cfg.num_splits, self.num_agents, 1)
+        self.act_shms = [[
+            torch.zeros(act_shape, dtype=torch.int32).share_memory_().numpy() for _ in range(self.cfg.num_splits)
+        ] for _ in range(self.cfg.num_actors)]
+        self.act_semaphores = [[multiprocessing.Semaphore(0) for _ in range(self.cfg.num_splits)]
+                               for _ in range(self.cfg.num_actors)]
+
+        # TODO: initialize env step outputs using config
+        # following is just the case of StarCraft2 (policy-sharing environments)
+        keys = ['obs', 'share_obs', 'rewards', 'available_actions', 'fct_masks', 'rnn_states', 'rnn_states_critic']
+        self.envstep_output_shms = []
+        for _ in range(self.cfg.num_policy_workers):
+            shms = []
+            for _ in range(self.cfg.num_splits):
+                shm_dict = {}
+                for k in keys:
+                    if not hasattr(self.buffer, k):
+                        continue
+                    shape = getattr(self.buffer, k).shape[2:]
+                    shm_dict[k] = torch.zeros(shape, dtype=torch.float32).share_memory_().numpy()
+                shm_dict['dones'] = torch.zeros(self.buffer.masks.shape[2:],
+                                                dtype=torch.float32).share_memory_().numpy()
+                shms.append(shm_dict)
+            self.envstep_output_shms.append(shms)
+        self.envstep_output_semaphores = [[multiprocessing.Semaphore(0) for _ in range(self.cfg.num_splits)]
+                                          for _ in range(self.cfg.num_actors)]
+
+        self.policy_worker_ready_events = [multiprocessing.Event() for _ in range(self.cfg.num_policy_workers)]
+
+        self.policy_avg_stats = dict()
+
+        self.last_timing = dict()
+        self.env_steps = dict()
+        self.samples_collected = 0
+        self.total_env_steps_since_resume = 0
+
+        # currently this applies only to the current run, not experiment as a whole
+        # to change this behavior we'd need to save the state of the main loop to a filesystem
+        self.total_train_seconds = 0
+
+        self.last_report = time.time()
+        self.last_experiment_summaries = 0
+
+        self.report_interval = 5.0  # sec
+        self.log_interval = self.cfg.log_interval  # sec
+
+        self.avg_stats_intervals = (2, 12, 60)  # 10 seconds, 1 minute, 5 minutes
+
+        self.throughput_stats = [deque([], maxlen=stat_len) for stat_len in self.avg_stats_intervals]
+        self.avg_stats = dict()
+        self.stats = dict()  # regular (non-averaged) stats
+
+        # TODO: add summary writer
+
+    def initialize(self):
+        pass
+
+    def finalize(self):
+        pass
+
+    def create_actor_worker(self, idx, actor_queue):
+        num_actors_per_queue = self.cfg.num_actors // self.cfg.num_policy_workers
+        policy_worker_idx = idx // num_actors_per_queue
+        print('#####################', policy_worker_idx)
+        return ActorWorker(
+            self.cfg,
+            self.env_fn,
+            self.num_agents,
+            idx,
+            self.buffer,
+            task_queue=actor_queue,
+            policy_queue=self.policy_queues[policy_worker_idx],
+            report_queue=self.report_queue,
+            act_shm=self.act_shms[idx],
+            act_semaphore=self.act_semaphores[idx],
+            envstep_output_shm=self.envstep_output_shms[policy_worker_idx],
+            envstep_output_semaphore=self.envstep_output_semaphores[idx],
+        )
+
+    # noinspection PyProtectedMember
+    def init_subset(self, indices, actor_queues):
+        """
+        Initialize a subset of actor workers (rollout workers) and wait until the first reset() is completed for all
+        envs on these workers.
+
+        This function will retry if the worker process crashes during the initial reset.
+
+        :param indices: indices of actor workers to initialize
+        :param actor_queues: task queues corresponding to these workers
+        :return: initialized workers
+        """
+
+        reset_timelimit_seconds = self.cfg.reset_timeout_seconds
+
+        workers = dict()
+        last_env_initialized = dict()
+        for i in indices:
+            w = self.create_actor_worker(i, actor_queues[i])
+            w.start_process()
+            w.init()
+            w.request_reset()
+            workers[i] = w
+            last_env_initialized[i] = time.time()
+
+        total_num_envs = self.cfg.num_actors * self.cfg.envs_per_actor
+        envs_initialized = [0] * self.cfg.num_actors
+        workers_finished = set()
+
+        while len(workers_finished) < len(workers):
+            failed_worker = -1
+
+            try:
+                report = self.report_queue.get(timeout=1.0)
+
+                if 'initialized_env' in report:
+                    worker_idx, split_idx = report['initialized_env']
+                    last_env_initialized[worker_idx] = time.time()
+                    envs_initialized[worker_idx] += 1
+
+                    log.debug(
+                        'Progress for %d workers: %d/%d envs initialized...',
+                        len(indices),
+                        sum(envs_initialized),
+                        total_num_envs,
+                    )
+                elif 'finished_reset' in report:
+                    workers_finished.add(report['finished_reset'])
+                elif 'critical_error' in report:
+                    failed_worker = report['critical_error']
+            except Empty:
+                pass
+
+            for worker_idx, w in workers.items():
+                if worker_idx in workers_finished:
+                    continue
+
+                time_passed = time.time() - last_env_initialized[worker_idx]
+                timeout = time_passed > reset_timelimit_seconds
+
+                if timeout or failed_worker == worker_idx or not w.process.is_alive():
+                    envs_initialized[worker_idx] = 0
+
+                    log.error('Worker %d is stuck or failed (%.3f). Reset!', w.worker_idx, time_passed)
+                    log.debug('Status: %r', w.process.is_alive())
+                    stuck_worker = w
+                    stuck_worker.process.kill()
+
+                    new_worker = self.create_actor_worker(worker_idx, actor_queues[worker_idx])
+                    new_worker.start_process()
+                    new_worker.init()
+                    new_worker.request_reset()
+
+                    last_env_initialized[worker_idx] = time.time()
+                    workers[worker_idx] = new_worker
+                    del stuck_worker
+
+        return workers.values()
+
+    # noinspection PyUnresolvedReferences
+    def init_workers(self):
+        """
+        Initialize all types of workers and start their worker processes.
+        """
+
+        actor_queues = [MpQueue(2 * 1000 * 1000) for _ in range(self.cfg.num_actors)]
+
+        policy_worker_queues = []
+        for i in range(self.cfg.num_policy_workers):
+            policy_worker_queues.append(TorchJoinableQueue())
+
+        log.info('Initializing learners...')
+        policy_lock = multiprocessing.Lock()
+        resume_experience_collection_cv = multiprocessing.Condition()
+
+        log.info('Initializing policy workers...')
+
+        num_actors_per_policy_worker = self.cfg.num_actors // self.cfg.num_policy_workers
+        for i in range(self.cfg.num_policy_workers):
+            actor_slice = slice(i * num_actors_per_policy_worker, (i + 1) * num_actors_per_policy_worker)
+            policy_worker = PolicyWorker(
+                i,
+                self.cfg,
+                self.obs_space,
+                self.share_obs_space,
+                self.action_space,
+                self.buffer,
+                self.policy_queues[i],
+                actor_queues[actor_slice],
+                self.report_queue,
+                policy_worker_queues[i],
+                policy_lock,
+                resume_experience_collection_cv,
+                self.act_shms[actor_slice],
+                self.act_semaphores[actor_slice],
+                self.envstep_output_shms[i],
+                self.envstep_output_semaphores[actor_slice],
+                self.policy_worker_ready_events[i],
+            )
+            self.policy_workers.append(policy_worker)
+            policy_worker.start_process()
+
+        log.info('Initializing actors...')
+
+        # We support actor worker initialization in groups, which can be useful for some envs that
+        # e.g. crash when too many environments are being initialized in parallel.
+        # Currently the limit is not used since it is not required for any envs supported out of the box,
+        # so we parallelize initialization as hard as we can.
+        # If this is required for your environment, perhaps a better solution would be to use global locks,
+        # like FileLock (see doom_gym.py)
+        max_parallel_init = 32  # might be useful to limit this for some envs
+        actor_indices = list(range(self.cfg.num_actors))
+        for i in range(0, self.cfg.num_actors, max_parallel_init):
+            workers = self.init_subset(actor_indices[i:i + max_parallel_init], actor_queues)
+            self.actor_workers.extend(workers)
+
+    def init_transmitters(self):
+        transmitter_queues = []
+        for i in range(self.cfg.num_transmitters):
+            transmitter_queues.append(TorchJoinableQueue())
+
+        for idx in range(self.cfg.num_transmitters):
+            t = Transmitter(self.cfg, idx, transmitter_queues[i], self.buffer, self.policy_worker_ready_events)
+            t.init()
+            self.transmitters.append(t)
+
+    def finish_initialization(self):
+        """Wait until policy workers are fully initialized."""
+        for w in self.policy_workers:
+            log.debug('Waiting for policy worker %d to finish initialization...', w.worker_idx)
+            w.init()
+            log.debug('Policy worker %d initialized!', w.worker_idx)
+
+    def process_report(self, report):
+        """Process stats from various types of workers."""
+
+        if 'samples' in report:
+            self.samples_collected += report['samples']
+
+        if 'timing' in report:
+            for k, v in report['timing'].items():
+                if k not in self.avg_stats:
+                    self.avg_stats[k] = deque([], maxlen=50)
+                self.avg_stats[k].append(v)
+
+        if 'stats' in report:
+            self.stats.update(report['stats'])
+
+    def report(self):
+        """
+        Called periodically (every X seconds, see report_interval).
+        Print experiment stats (FPS, avg rewards) to console and dump TF summaries collected from workers to disk.
+        """
+        now = time.time()
+        sample_throughputs = []
+        for throughput_stat in self.throughput_stats:
+            throughput_stat.append((now, self.samples_collected))
+            if len(throughput_stat) > 1:
+                past_moment, past_samples = throughput_stat[0]
+                sample_throughput = (self.samples_collected - past_samples) / (now - past_moment)
+            else:
+                sample_throughput = math.nan
+            sample_throughputs.append(sample_throughput)
+
+        self.print_stats(sample_throughputs)
+
+        if time.time() - self.last_experiment_summaries > self.log_interval:
+            # TODO: write to wandb/TensorBoard
+            pass
+
+    def print_stats(self, sample_throughputs):
+        log.debug('Throughput: {:.2f} (10 sec), {:.2f} (1 min), {:.2f} (5 mins). Samples: {}.'.format(
+            *sample_throughputs, self.samples_collected))
+
+        # TODO: episodic summary, e.g. reward & winning rate
+        # if 'reward' in self.policy_avg_stats:
+        #     policy_reward_stats = []
+        #     reward_stats = self.policy_avg_stats['reward']
+        #     if len(reward_stats) > 0:
+        #         policy_reward_stats.append(f'{np.mean(reward_stats):.3f}')
+        #     log.debug('Avg episode reward: %r', policy_reward_stats)
+
+    def _should_end_training(self):
+        end = len(self.env_steps) > 0 and all(s > self.cfg.train_for_env_steps for s in self.env_steps.values())
+        end |= self.total_train_seconds > self.cfg.train_for_seconds
+
+        if self.cfg.benchmark:
+            end |= self.total_env_steps_since_resume >= int(2e6)
+            end |= sum(self.samples_collected) >= int(1e6)
+
+        return end
+
+    def run(self):
+        """
+        This function contains the main loop of the algorithm, as well as initialization/cleanup code.
+
+        :return: ExperimentStatus (SUCCESS, FAILURE, INTERRUPTED). Useful in testing.
+        """
+
+        status = ExperimentStatus.SUCCESS
+
+        self.init_transmitters()
+        self.init_workers()
+        self.finish_initialization()
+
+        log.info('Collecting experience...')
+
+        timing = Timing()
+        with timing.timeit('experience'):
+            # noinspection PyBroadException
+            try:
+                while not self._should_end_training():
+                    try:
+                        reports = self.report_queue.get_many(timeout=0.1)
+                        for report in reports:
+                            self.process_report(report)
+                    except Empty:
+                        pass
+
+                    if time.time() - self.last_report > self.report_interval:
+                        self.report()
+
+                        now = time.time()
+                        self.total_train_seconds += now - self.last_report
+                        self.last_report = now
+
+            except Exception:
+                log.exception('Exception in driver loop')
+                status = ExperimentStatus.FAILURE
+            except KeyboardInterrupt:
+                log.warning('Keyboard interrupt detected in driver loop, exiting...')
+                status = ExperimentStatus.INTERRUPTED
+
+        all_workers = self.actor_workers + self.policy_workers
+
+        child_processes = list_child_processes()
+
+        time.sleep(0.1)
+        log.debug('Closing workers...')
+        for i, w in enumerate(all_workers):
+            w.close()
+            time.sleep(0.01)
+        for i, w in enumerate(all_workers):
+            w.join()
+        log.debug('Workers joined!')
+
+        time.sleep(0.1)
+        log.debug('Closing Transmitters...')
+        for t in self.transmitters:
+            t.close()
+            time.sleep(0.02)
+        for t in self.transmitters:
+            t.join()
+        log.debug('Transmitters joined!')
+
+        # VizDoom processes often refuse to die for an unidentified reason, so we're force killing them with a hack
+        kill_processes(child_processes)
+
+        fps = self.total_env_steps_since_resume / timing.experience
+        log.info('Collected %r, FPS: %.1f', self.env_steps, fps)
+        log.info('Timing: %s', timing)
+
+        # if self._should_end_training():
+        #     with open(done_filename(self.cfg), 'w') as fobj:
+        #         fobj.write(f'{self.env_steps}')
+
+        time.sleep(0.5)
+        log.info('Done!')
+
+        return status
