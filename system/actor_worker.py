e6f0bd21297a33ef4a02cf2e3c7bb142590ac886
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2022-10-13 20:56:59 +0800

    update

--- system/actor_worker.py
+++ system/actor_worker.py
@@ -321,8 +321,7 @@ class ActorWorker:
         with timing.add_time('env_step/simulation'), timing.time_avg('env_step/simulation_avg'):
             envstep_outputs = flatten_recurrent(env.step(self.act_shm[split_idx], env_set_tasks))
             if env_set_tasks is not None:
-                print('observation_self', envstep_outputs['observation_self'].shape)
-                print('box_obs', envstep_outputs['box_obs'].shape)
+                print('observation_self', envstep_outputs['observation_self'][0])
                 print('env_set_tasks', env_set_tasks[0], 'len', len(env_set_tasks))
             self.debug_ep_steps[split_idx] += 1
 
